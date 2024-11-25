ad412a7e3b80c5e884b5ae8c30365badf04a40c8
Wellington Dos Santos Vieira <welldssvieira@gmail.com>
2024-11-24 22:21:46 -0300

    Implementação do toque ao mudar de rotina.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,7 @@
-// Supondo que você tenha um botão ou elemento para alternar as rotinas
-
-
 function mudarRotina() {
     const rotinaFolga = document.querySelector("#atividades .atividades.folga");
     const rotinaUtil = document.querySelector("#atividades .atividades.util");
 
-    // Alterna entre as rotinas
     if (rotinaUtil.style.display === "none"){
         rotinaUtil.style.display = "flex";
         rotinaFolga.style.display = "none";
@@ -13,4 +9,11 @@ function mudarRotina() {
         rotinaUtil.style.display = "none";
         rotinaFolga.style.display = "flex";
     }
-}
+
+        const audio = document.getElementById('audio');
+        audio.play().catch(error => {
+            console.log('Erro ao reproduzir o som:', error);
+        });
+    };
+
+
