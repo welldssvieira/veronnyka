445b43db21f3712058daddc7a9b255c52b6e7b81
Wellington Dos Santos Vieira <welldssvieira@gmail.com>
2024-11-26 08:53:16 -0300

    Atualização das rotinas, implementação de icones da rotina útil e correção de bugs.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,20 @@
+window.onload = function() {
+    mudarRotina();
+};
+
 function mudarRotina() {
     const rotinaFolga = document.querySelector("#atividades .atividades.folga");
     const rotinaUtil = document.querySelector("#atividades .atividades.util");
+    const nomeRotina = document.querySelector("#dia .rotina_atual span");
 
     if (rotinaUtil.style.display === "none"){
         rotinaUtil.style.display = "flex";
         rotinaFolga.style.display = "none";
+        nomeRotina.innerHTML = "Rotina útil"
     } else {
         rotinaUtil.style.display = "none";
         rotinaFolga.style.display = "flex";
+         nomeRotina.innerHTML = "Rotina de folga"
     }
 
         const audio = document.getElementById('audio');
@@ -15,5 +22,7 @@ function mudarRotina() {
             console.log('Erro ao reproduzir o som:', error);
         });
     };
+
+
 
 
