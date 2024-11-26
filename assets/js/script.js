87a9d3fa177e22ecba3f27824cc0770b0b24da66
Wellington Dos Santos Vieira <welldssvieira@gmail.com>
2024-11-26 16:08:46 -0300

    Versão 0.0.1 do app Veronnyka.

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,8 +17,6 @@ function mudarRotina() {
         nomeRotina.innerHTML = "Rotina Folga";
     }
 
-
-
     const audio = document.getElementById('audio');
     audio.play().catch(error => {
         console.log('Erro ao reproduzir o som:', error);
@@ -40,6 +38,48 @@ window.onload = function dataAtual(){
     const dataInline = document.querySelector("#dia .dia_data");
     dataInline.innerHTML = `<span>${diasSemana} ,  ${dataHojeMes} de ${nomeMesAtual}</span>`
 }
+
+
+function obterHoraFormatada() {
+    const horaAtual = new Date();
+    
+    // Obtém a hora e os minutos
+    let horas = horaAtual.getHours();
+    let minutos = horaAtual.getMinutes();
+    
+    // Adiciona zero à esquerda se a hora ou minutos forem menores que 10
+    horas = horas < 10 ? '0' + horas : horas;
+    minutos = minutos < 10 ? '0' + minutos : minutos;
+    
+    // Retorna a hora no formato 15:40
+    return `${horas}:${minutos}`;
+}
+/*
+window.onload = function tarefaAtual() {
+    // Seleciona os elementos necessários
+    var boxTarefaAtual = document.querySelector("#card_principal .atividade_atual .atividade");
+    var iconeTarefaAtual = document.querySelector("#card_principal .atividade_atual .atividade .emoji img");
+    const tituloTarefaAtual = document.querySelector("#card_principal .atividade_atual .atividade .titulos .titulo p");
+    const subtituloTarefaAtual = document.querySelector("#atividades .atividades.util .atividade .titulos .subtitulo p");
+    const horaTarefaAtual = document.querySelector("#atividades .atividades.folga .atividade .tempo .minutos span");
+    const tempoTarefaAtual = document.querySelector("#card_principal .atividade_atual .atividade .tempo .texto span");
+    const atividade = document.querySelector("#atividades .atividades.folga .atividade");
+    const atividadeAcordar = document.querySelector("#atividades .atividades.folga .atividade.acordar");
+    const iconeAtividadeAcordar = document.querySelector("#atividades .atividade_atual .atividade.acordar .emoji img");
+    
+    // Pega o horário de início da atividade
+    const IntervaloInicio = document.querySelector("#atividades .atividades.folga .atividade .titulos .intervalo .intervalo_inicio p").textContent;
+
+    // Verifica se a hora das atividades é igual à hora atual
+    if (obterHoraFormatada() === IntervaloInicio) {
+        boxTarefaAtual = atividadeAcordar
+        iconeTarefaAtual.src = "media/icones/Icon_coração.png"
+    } else {
+        console.log("A hora das atividades NÃO é igual à hora atual.");
+    }
+};*/
+
+
 
 
 
